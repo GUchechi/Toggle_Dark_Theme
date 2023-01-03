0dddd080fd464187a2cfb7fd5563fcc2f741a871
GUchechi <godswilluchendu7@gmail.com>
2023-01-03 12:55:17 +0100

    finished html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import './App.css';
+import "./App.css";
+import useLocalStorage from "use-local-storage";
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="app">
+      <div className="login">
+        <h1>Login</h1>
+        <div className="container">
+          <div className="top">
+            <i class="fab fa-google"></i>
+            <i class="fab fa-facebook-square"></i>
+            <i class="fab fa-linkedin"></i>
+            <i class="fab fa-twitter-square"></i>
+            <i class="fab fa-apple"></i>
+          </div>
+          <p className="divider">
+            <span>Or</span>
+          </p>
+          <form>
+            <label>E-mail</label>
+            <input type="email" placeholder="Enter your email" required />
+            <label>Password</label>
+            <input type="password" placeholder="Enter your password" />
+            <div className="remember">
+              <input type="checkbox" checked="checked" />
+              <p>Remember Me</p>
+            </div>
+            <button>Log In</button>
+          </form>
+          <div className="bottom">
+            <p>Forget your password?</p>
+            <a href="/">Reset Password</a>
+          </div>
+          <p className="create">Create Account</p>
+        </div>
+        <div className="theme-toggle">
+          <h2>Light Theme</h2>
+          <i class="fas fa-toggle-on"></i>
+        </div>
+      </div>
     </div>
   );
 }
